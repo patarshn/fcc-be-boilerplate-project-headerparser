9926ece86c1385974332f6b996962c0b61b600bc
Patar Martua Doli Siahaan <38348364+patarshn@users.noreply.github.com>
2022-06-06 12:37:24 +0000

    create whoami api

--- index.js
+++ index.js
@@ -21,8 +21,13 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  console.log(req.headers.host);
+  res.json({
+    ipaddress: req.headers['x-forwarded-for'],
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent'],
+  })
 });
 
 
